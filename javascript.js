087b3bcd0b85e04a0b6de83795816a4c769030ef
Niko Shin <nshin17@gmail.com>
2022-05-08 22:33:39 -0700

    Edit .js with function that creates new divs with container class

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
-// selecting main div container in body:
-const divContainer = document.querySelector('.container');
+
+
+const divContainer = document.querySelector('.container'); // selecting main div container in body:
 const divGrid = document.createElement('div'); // create div element.
 const addDivGridClass = divGrid.classList.add('div-grid'); // adding new class 'div-grid' to the newly created 'div' element in divGrid variable.
 const appendDivGrid = divContainer.appendChild(divGrid); // append the div element to parent node.
 const classDivGrid = document.querySelectorAll('.div-grid'); // selecting ALL elements with the class name 'div-grid'.
 
 
-/// function that takes number parameter and displays div grids on web page:
-function createDivGrid() {
+
+
+// function - creates rows of divs:
+function createDivRows() {
     for (let i=1; i<=16; i++) {
         const divGrid = document.createElement('div'); // create div element.
         divGrid.classList.add('div-grid'); // add class name to div element.
         divContainer.appendChild(divGrid); // append the div element to parent node.
     }
 }
-createDivGrid();
+createDivRows();
+
+// Function - creates columns of divs:
+// 'num' parameter iterate the columns from 1 - 16 columns.
+function createDivColumns() {
+    for (let i=1; i<=16; i++) {
+        const body = document.querySelector('body');
+        const newDivs = document.createElement('div');
+        body.appendChild(newDivs).classList.add('container');
+    }
+}
+createDivColumns();
 
 
 // Changes background color if mouse hovers over element:
@@ -27,20 +41,5 @@ divContainer.addEventListener('mouseover', (event) => {
 divContainer.addEventListener('mouseout', (event) => {
     event.target.style.backgroundColor = 'white';
 })
-
-
-
-
-
-
-
-// // function - event listener that creates mouse hovering effect that changes background color by adding a class that connects to the CSS rule 'hover-effect':
-// hoverEffect.addEventListener('mouseover', (event) => {
-//     // add the #hover-effect as a new class to the div grids:
-//     // document.querySelector('.div-grid').classList.add('hover-effect');
-//     hoverEffect.classList.add('hover-effect');
-
-//     event.target.style.backgroundColor = 'purple';
-// })
 
 
