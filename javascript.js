c595215625c9dcde3e172fe996a11356fa625851
Niko Shin <nshin17@gmail.com>
2022-05-10 21:37:59 -0700

    Added function that creates the column and rows of the grid

--- javascript.js
+++ javascript.js
@@ -1,45 +1,56 @@
 
-
+// SELECTORS:
 const divContainer = document.querySelector('.container'); // selecting main div container in body:
 const divGrid = document.createElement('div'); // create div element.
-const addDivGridClass = divGrid.classList.add('div-grid'); // adding new class 'div-grid' to the newly created 'div' element in divGrid variable.
-const appendDivGrid = divContainer.appendChild(divGrid); // append the div element to parent node.
-const classDivGrid = document.querySelectorAll('.div-grid'); // selecting ALL elements with the class name 'div-grid'.
+const classDivGrid = document.querySelectorAll('.div-grid'); // selecting ALL elements with the class name '.div-grid'.
+const selectClassCell = document.querySelector('.cell'); // select all div with class '.cell'.
 
 
 
 
-// function - creates rows of divs:
-function createDivRows() {
-    for (let i=1; i<=16; i++) {
-        const divGrid = document.createElement('div'); // create div element.
-        divGrid.classList.add('div-grid'); // add class name to div element.
-        divContainer.appendChild(divGrid); // append the div element to parent node.
+
+// Create entire grid with input from user:
+function createGrid(num) {
+    const columns = createDivColumns(num);
+    const rows = createDivRows(num);
+}
+createGrid(16);
+
+
+
+
+
+// Create Columns:
+function createDivColumns(num) {
+    for (let i=1; i<=num; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        divContainer.appendChild(div);
     }
 }
-createDivRows();
 
-// Function - creates columns of divs:
-// 'num' parameter iterate the columns from 1 - 16 columns.
-function createDivColumns() {
-    for (let i=1; i<=16; i++) {
-        const body = document.querySelector('body');
-        const newDivs = document.createElement('div');
-        body.appendChild(newDivs).classList.add('container');
+// Create rows:
+function createDivRows(num) {
+    const selectAllDivs = document.querySelectorAll('.grid'); // select all divs with the class '.grid'.
+    for (let i=0; i<num; i++) {
+        for (div of selectAllDivs) {
+            const newDiv = document.createElement('div'); // create div element.
+            newDiv.classList.add('cell'); // add class name to div element.
+            div.appendChild(newDiv);
+        }
     }
 }
-createDivColumns();
 
 
+
+// EVENT LISTENERS:
 // Changes background color if mouse hovers over element:
 divContainer.addEventListener('mouseover', (event) => {
-    // button.classList.add('hover-effect');
-    event.target.style.backgroundColor = 'purple';
+    event.target.style.backgroundColor = 'aquamarine';
 })
 
-// Changes background color if mouse exits the element:
+// Revert to original background color with mouse exit:
 divContainer.addEventListener('mouseout', (event) => {
     event.target.style.backgroundColor = 'white';
 })
-
 
